46727e44107310772eb0300822ee2c310f827799
ahip2 <ahip2@cam.ac.uk>
2022-02-07 21:38:50 +0000

    test

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,30 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from cProfile import label
+from tkinter import Y
+from numpy import sqrt
+from sqlalchemy import lateral
+from zmq import PUSH
+from floodsystem.stationdata import build_station_list
+
+#emply list
+swr=[]
+
+def stations_within_radius (centre, r) :
+    #list of stations
+    stations = build_station_list
+    #finding stations distance
+    for station in stations:
+        station.coord == long, lat
+        x,y = centre
+        dis = sqrt(((x - long)**2 )+((y-lat)**2))
+        #if distance is within the radius add to the list
+        if dis < r:
+            swr.append(station.name)
+    return swr
+
+sorted_swr = sorted(swr)
+print(sorted_swr)
+
+PUSH
