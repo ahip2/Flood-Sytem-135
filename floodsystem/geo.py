b7ac4aeaf00c7ae54d7fb42573c00710af64a173
ahip2 <ahip2@cam.ac.uk>
2022-02-10 10:04:53 +0000

    final

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,31 +5,56 @@
 geographical data.
 
 """
+from socket import SO_BROADCAST
+from unicodedata import name
 
-from cProfile import label
-from tkinter import Y
-from numpy import sqrt
-from sqlalchemy import lateral
-from zmq import PUSH
-from floodsystem.stationdata import build_station_list
+from sklearn.metrics import nan_euclidean_distances
+from floodsystem.station import MonitoringStation
+from numpy import number, sqrt
+from .stationdata import build_station_list
+from .utils import sorted_by_key
 
-#emply list
+
+#empty list
 swr=[]
 
-def stations_within_radius (centre, r) :
-    #list of stations
-    stations = build_station_list
+
+def stations_within_radius (stations, centre, r) :
     #finding stations distance
     for station in stations:
-        station.coord == long, lat
+        MonitoringStation.coord= (lat,long)
         x,y = centre
-        dis = sqrt(((x - long)**2 )+((y-lat)**2))
+        dis = sqrt(((x - lat)**2 )+((y-long)**2))
         #if distance is within the radius add to the list
         if dis < r:
             swr.append(station.name)
+        else:
+            pass
     return swr
 
 sorted_swr = sorted(swr)
 print(sorted_swr)
 
-PUSH
+def rivers_by_station_number(stations, N): 
+
+    #list of rivers and the number of stations in paired entries
+    list=[river_name,num_of_stations]
+    N_list=[]
+
+    for i in rivers_with_station(stations):
+        station_by_river(i)=num_of_stations
+        i=river_name
+        list.append(river_name,num_of_stations)
+        list.sort(key=lambda x:x[1])
+    return list
+    #we now have a list of river names and the number of stations
+    #sorted in order of number of station
+    N_list= list[-N:]
+    list[-N]= name1,num1
+    if list[river_name]=name1: 
+        pass
+    elif list[num_of_stations]=num1 :
+        N_list.append(river_name,num_of_stations)
+
+return N_list
+    
