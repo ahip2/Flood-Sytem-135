f2a60457670a6def5f5f70ac4df74a1b2364e920
John Carr <jc2336@cam.ac.uk>
2022-03-01 21:58:07 +0000

    Task 2E retry

--- Task2E.py
+++ Task2E.py
@@ -10,6 +10,39 @@ from datetime import datetime
 
 from floodsystem.datafetcher import fetch_measure_levels
 
+import datetime
+
+from floodsystem.flood import plot_water_levels, stations_highest_rel_level
+
+from floodsystem.datafetcher import fetch_measure_levels
+
+from floodsystem.stationdata import build_station_list
+
+import datetime
+
+
+def run():
+
+    stations=build_station_list()
+
+    s=stations_highest_rel_level(stations,5)
+
+    #gets top 5 relative levelled stations
+
+    dt = 10
+
+    for station in s:
+
+        #iterates over each station and plots a graph using dates+levels for each one
+
+        dates, levels = fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=dt))
+
+        plot_water_levels(station,dates,levels)
+
+
+if __name__ == "__main__":
+
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
 
 def plot_water_levels(station, dates, levels):
 
@@ -44,37 +77,3 @@ def plot_water_levels(station, dates, levels):
             plt.axhline(station.typical_range[1], color='r') # upper level
 
     plt.show()
-
-    import datetime
-
-from floodsystem.flood import plot_water_levels, stations_highest_rel_level
-
-from floodsystem.datafetcher import fetch_measure_levels
-
-from floodsystem.stationdata import build_station_list
-
-import datetime
-
-
-def run():
-
-    stations=build_station_list()
-
-    s=stations_highest_rel_level(stations,5)
-
-    #gets top 5 relative levelled stations
-
-    dt = 10
-
-    for station in s:
-
-        #iterates over each station and plots a graph using dates+levels for each one
-
-        dates, levels = fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=dt))
-
-        plot_water_levels(station,dates,levels)
-
-
-if __name__ == "__main__":
-
-    print("*** Task 2E: CUED Part IA Flood Warning System ***")
