d9f0c25d88590ae3b4fec3967760d7b67f2c0bf2
John Carr <jc2336@cam.ac.uk>
2022-03-02 12:41:55 +0000

    plot.py commit

--- Task2E.py
+++ Task2E.py
@@ -1,25 +1,12 @@
 from floodsystem.stationdata import build_station_list
 
-from floodsystem.utils import sorted_by_key
-
-from floodsystem.stationdata import update_water_levels
-
-import matplotlib.pyplot as plt
-
 from datetime import datetime
 
 from floodsystem.datafetcher import fetch_measure_levels
 
-import datetime
+from floodsystem.flood import stations_highest_rel_level
 
-from floodsystem.flood import plot_water_levels, stations_highest_rel_level
-
-from floodsystem.datafetcher import fetch_measure_levels
-
-from floodsystem.stationdata import build_station_list
-
-import datetime
-
+from floodsystem.plot import plot_water_levels
 
 def run():
 
@@ -44,36 +31,3 @@ if __name__ == "__main__":
 
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
 
-def plot_water_levels(station, dates, levels):
-
- 
-
-    t =dates
-
-    level = levels
-
-    # Plot
-
-    plt.plot(t, level)
-
-    plt.xlabel('date')
-
-    plt.ylabel('water level (m)')
-
-    plt.xticks(rotation=45);
-
-    plt.title("Station {}".format(station))
-
- 
-
-    # Display plot
-
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
-
-    if station.typical_range != None:
-
-            plt.axhline(station.typical_range[0], color='r') # lower level
-
-            plt.axhline(station.typical_range[1], color='r') # upper level
-
-    plt.show()
