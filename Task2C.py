c8f4dffb824fb1578268207c9c7be3dd38662b9b
John Carr <jc2336@cam.ac.uk>
2022-03-02 12:46:54 +0000

    Task 2C working

--- Task2C.py
+++ Task2C.py
@@ -1,14 +1,31 @@
-
-from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list
 
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+
+from floodsystem.flood import stations_high_rel_level
+
+from floodsystem.utils import sorted_by_key
+
+ 
+
 def run():
-    
-    stations=build_station_list
-    N=10
-    
-    print(stations_highest_rel_level(stations,N))
-    
+
+    """Requirements for Task 2C"""
+
+    stations= build_station_list()
+
+    for s in stations_high_rel_level(stations,10):
+
+        relative_level=s.latest_level-s.typical_range[1]
+
+        print(s.name, relative_level)
+
+   
+
+ 
+
 if __name__ == "__main__":
+
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
+
     run()
